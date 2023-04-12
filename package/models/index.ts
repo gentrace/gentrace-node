8350d7cfe5341cd959a27eb7f63336469c5980cd
Vivek Nair <vivek@pesto.app>
2023-04-12 14:34:48 -0400

    chore: updating OpenAI generated NodeJS logic to "0.3.7" (#37)
    Co-authored-by: Gentrace Bot <bot@gentrace.ai>

--- package/models/index.ts
+++ package/models/index.ts
@@ -3,5 +3,4 @@ export * from './feedback-response';
 export * from './pipeline-run-request';
 export * from './pipeline-run-request-step-runs-inner';
 export * from './pipeline-run-request-step-runs-inner-provider';
-export * from './pipeline-run-request-step-runs-inner-provider-model-params-value';
 export * from './pipeline-run-response';
