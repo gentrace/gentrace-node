5b5ababc3e4cf7dfe2ae2b3ae8b37356a34dafad
Vivek Nair <vivek@pesto.app>
2023-05-05 16:47:05 -0400

    fix: temporarily duplicate source into openai and pinecone (#91)
    This fix is to support older versions of Node.JS and  (mainly) to get tsconfig's `moduleResolution: "node"` semantics to work correctly without changing to `moduleResolution: "nodenext"`

---
+++ package/openai/index.ts
@@ -0,0 +1,68 @@
+import {
+  Configuration as OpenAIConfiguration,
+  ConfigurationParameters as OpenAIConfigurationParameters,
+} from "openai";
+import { Configuration as GentraceConfiguration } from "./configuration";
+import { OpenAIPipelineHandler } from "./providers/llms/openai";
+
+class ModifiedOpenAIConfiguration extends OpenAIConfiguration {
+  gentraceApiKey: string;
+  gentraceBasePath?: string;
+  gentraceLogger?: {
+    info: (message: string, context?: any) => void;
+    warn: (message: string | Error, context?: any) => void;
+  };
+
+  constructor(
+    modifiedOAIConfig: OpenAIConfigurationParameters & {
+      gentraceApiKey: string;
+      gentraceBasePath?: string;
+      gentraceLogger?: {
+        info: (message: string, context?: any) => void;
+        warn: (message: string | Error, context?: any) => void;
+      };
+    }
+  ) {
+    super(modifiedOAIConfig);
+    this.gentraceApiKey = modifiedOAIConfig.gentraceApiKey;
+    this.gentraceBasePath = modifiedOAIConfig.gentraceBasePath;
+    this.gentraceLogger = modifiedOAIConfig.gentraceLogger;
+  }
+}
+
+class OpenAIApi extends OpenAIPipelineHandler {
+  constructor(modifiedOAIConfig: ModifiedOpenAIConfiguration) {
+    if (!modifiedOAIConfig.apiKey) {
+      throw new Error("API key not provided.");
+    }
+
+    if (!modifiedOAIConfig.gentraceApiKey) {
+      throw new Error("Gentrace API key not provided.");
+    }
+
+    if (modifiedOAIConfig.gentraceBasePath) {
+      try {
+        const url = new URL(modifiedOAIConfig.gentraceBasePath);
+        if (url.pathname.startsWith("/api/v1")) {
+        } else {
+          throw new Error('Gentrace base path must end in "/api/v1".');
+        }
+      } catch (err) {
+        throw new Error(`Invalid Gentrace base path: ${err.message}`);
+      }
+    }
+
+    const gentraceConfig = new GentraceConfiguration({
+      apiKey: modifiedOAIConfig.gentraceApiKey,
+      basePath: modifiedOAIConfig.gentraceBasePath,
+      logger: modifiedOAIConfig.gentraceLogger,
+    });
+
+    super({
+      config: modifiedOAIConfig,
+      gentraceConfig,
+    });
+  }
+}
+
+export { OpenAIApi, ModifiedOpenAIConfiguration as Configuration };
---
+++ package/openai/providers/utils.ts
@@ -0,0 +1,3 @@
+export type OptionalPipelineId = {
+  pipelineId?: string;
+};
