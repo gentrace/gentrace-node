d0d5b3340d6c10c9acac95d743b4d666f68b4794
Vivek Nair <vnair611@gmail.com>
2023-05-06 16:26:31 -0400

    fix: wip

---
+++ package/tests/completion.test.ts
@@ -0,0 +1,83 @@
+import { rest } from "msw";
+import { setupServer, SetupServer } from "msw/node";
+import { Configuration, OpenAIApi } from "../openai";
+
+describe("test_openai_completion_pipeline", () => {
+  const completionResponse = {
+    choices: [{ finish_reason: "stop", index: 0, text: "\n" }],
+    created: 1682109134,
+    id: "cmpl-77riQulvtyXo30e14QwSxzGATk2a5",
+    model: "text-davinci-003",
+    object: "text_completion",
+    usage: { completion_tokens: 1, prompt_tokens: 3, total_tokens: 4 },
+  };
+
+  const gentracePipelineRunResponse = {
+    pipelineRunId: "1f2e8493-5fd1-4359-8cd7-867175d6d9aa",
+  };
+
+  let server: SetupServer;
+
+  beforeAll(() => {
+    server = setupServer(
+      rest.post("https://api.openai.com/v1/completions", (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.set("Content-Type", "application/json"),
+          ctx.json(completionResponse)
+        );
+      }),
+
+      rest.post("https://gentrace.ai/api/v1/pipeline-run", (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.set("Content-Type", "application/json"),
+          ctx.json(gentracePipelineRunResponse)
+        );
+      })
+    );
+    server.listen();
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it("should return pipelineRunId when completion is given a pipelineId", async () => {
+    const openai = new OpenAIApi(
+      new Configuration({
+        gentraceApiKey: process.env.GENTRACE_API_KEY ?? "",
+        apiKey: process.env.OPENAI_KEY,
+      })
+    );
+
+    const completionResponse = await openai.createCompletion({
+      model: "text-davinci-003",
+      promptTemplate: "Write a brief summary of the history of {{ company }}: ",
+      promptInputs: {
+        company: "Google",
+      },
+      pipelineId: "test-completion-response",
+    });
+
+    expect(completionResponse.pipelineRunId).toMatch(/[0-9a-fA-F-]{36}/);
+  });
+
+  it("should not return pipelineRunId when completion is not given a pipeline", async () => {
+    const openai = new OpenAIApi(
+      new Configuration({
+        gentraceApiKey: process.env.GENTRACE_API_KEY ?? "",
+        apiKey: process.env.OPENAI_KEY,
+      })
+    );
+    const completionResponse = await openai.createCompletion({
+      model: "text-davinci-003",
+      promptTemplate: "Write a brief summary of the history of {{ company }}: ",
+      promptInputs: {
+        company: "Google",
+      },
+    });
+
+    expect(completionResponse.pipelineRunId).toBeUndefined();
+  });
+});
---
+++ package/tests/usage.test.ts
@@ -0,0 +1,122 @@
+import { Configuration, OpenAIApi } from "../openai";
+
+describe("Usage of OpenAIApi", () => {
+  describe("constructor", () => {
+    it("should create an instance when configuration is valid (gentrace.ai host)", () => {
+      expect(() => {
+        new OpenAIApi(
+          new Configuration({
+            gentraceApiKey: "gentrace-api-key",
+            gentraceBasePath: "https://gentrace.ai/api/v1/pipeline-run",
+            apiKey: "openai-api-key",
+          })
+        );
+      }).not.toThrow();
+    });
+
+    it("should create an instance when configuration is valid (gentrace.ai host)", () => {
+      expect(() => {
+        new OpenAIApi(
+          new Configuration({
+            gentraceApiKey: "gentrace-api-key",
+            apiKey: "openai-api-key",
+          })
+        );
+      }).not.toThrow();
+    });
+
+    it("should create an instance when configuration is valid (staging.gentrace.ai host)", () => {
+      expect(() => {
+        new OpenAIApi(
+          new Configuration({
+            gentraceApiKey: "gentrace-api-key",
+            gentraceBasePath: "https://staging.gentrace.ai/api/v1/pipeline-run",
+            apiKey: "openai-api-key",
+          })
+        );
+      }).not.toThrow();
+    });
+
+    it("should create an instance when configuration is valid (localhost + port host)", () => {
+      expect(() => {
+        new OpenAIApi(
+          new Configuration({
+            gentraceApiKey: "gentrace-api-key",
+            gentraceBasePath: "http://localhost:3000/api/v1/pipeline-run",
+            apiKey: "openai-api-key",
+          })
+        );
+      }).not.toThrow();
+    });
+
+    it("should create an instance when configuration is valid (no host)", () => {
+      expect(() => {
+        new OpenAIApi(
+          new Configuration({
+            gentraceApiKey: "gentrace-api-key",
+            apiKey: "openai-api-key",
+          })
+        );
+      }).not.toThrow();
+    });
+
+    it("should create an instance when configuration is valid", () => {
+      const openai = new OpenAIApi(
+        new Configuration({
+          gentraceApiKey: "gentrace-api-key",
+          gentraceBasePath: "https://gentrace.ai/api/v1",
+          apiKey: "openai-api-key",
+        })
+      );
+      expect(openai).toBeDefined();
+    });
+
+    it("should throw an error when API key is absent", () => {
+      expect(() => {
+        new OpenAIApi(
+          new Configuration({
+            gentraceApiKey: "gentrace-api-key",
+            gentraceBasePath: "https://gentrace.ai/api/v1",
+            apiKey: "",
+          })
+        );
+      }).toThrow("API key not provided.");
+    });
+
+    it("should throw an error when Gentrace API key is absent", () => {
+      expect(() => {
+        new OpenAIApi(
+          new Configuration({
+            gentraceApiKey: "",
+            gentraceBasePath: "https://gentrace.ai/api/v1",
+            apiKey: "openai-api-key",
+          })
+        );
+      }).toThrow("Gentrace API key not provided.");
+    });
+
+    it("should throw an error when Gentrace base path is invalid", () => {
+      expect(() => {
+        new OpenAIApi(
+          new Configuration({
+            gentraceApiKey: "gentrace-api-key",
+            gentraceBasePath: "invalid-url",
+            apiKey: "openai-api-key",
+          })
+        );
+      }).toThrow("Invalid Gentrace base path");
+    });
+
+    it("should throw an error when Gentrace base path does not end in /api/v1", () => {
+      expect(() => {
+        new OpenAIApi(
+          new Configuration({
+            gentraceApiKey: "gentrace-api-key",
+            gentraceBasePath: "https://gentrace.ai/invalid-path",
+            apiKey: "openai-api-key",
+          })
+        );
+      }).toThrow('Gentrace base path must end in "/api/v1".');
+    });
+  });
+});
