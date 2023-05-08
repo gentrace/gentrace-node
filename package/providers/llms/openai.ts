54a8dfd861a2316bcbcdb851959463d18b428db4
Vivek Nair <vnair611@gmail.com>
2023-05-08 06:22:00 -0400

    fix: wip

--- package/providers/llms/openai.ts
+++ package/providers/llms/openai.ts
@@ -1,6 +1,6 @@
 import { AxiosRequestConfig, AxiosResponse } from "openai/node_modules/axios";
 // @ts-ignore: the typings from @types/mustache are incorrect
-import Mustache from "mustache";
+import * as Mustache from "mustache";
 import {
   ChatCompletionRequestMessage,
   CreateChatCompletionRequest,
