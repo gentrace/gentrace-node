bac590c0f1bdde67c4e1bbb69ffb13be4ef771ba
Vivek Nair <vivek@pesto.app>
2023-05-09 11:31:57 -0400

    fix: ensure CJS dependencies are properly converted (#105)

--- package/rollup.config.esm.ts
+++ package/rollup.config.esm.ts
@@ -1,4 +1,7 @@
 import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
 
 export default {
   input: ["index.ts", "openai.ts", "pinecone.ts"],
@@ -12,6 +15,9 @@ export default {
     },
   ],
   plugins: [
+    json(),
+    commonjs(),
+    nodeResolve(),
     typescript({
       tsconfig: "./tsconfig.esm.json",
     }),
