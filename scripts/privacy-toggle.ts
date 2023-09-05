3601617e2f050cfcf9a0428c52bb80447f7bf661
Vivek Nair <vnair611@gmail.com>
2023-09-05 17:12:04 -0400

    fix: fix issues with privacy toggle

--- scripts/privacy-toggle.ts
+++ scripts/privacy-toggle.ts
@@ -3,11 +3,8 @@ import { join } from "path";
 import { getPackages } from "@manypkg/get-packages";
 
 function writeOpenAIPackageJson(path: string, privacy: boolean) {
-  // Define the path to the package.json file.
-  const packageJsonPath = join(__dirname, path);
-
   // Read the package.json file.
-  const packageJsonString = readFileSync(packageJsonPath, "utf8");
+  const packageJsonString = readFileSync(path, "utf8");
   const packageJson = JSON.parse(packageJsonString);
 
   if (privacy) {
@@ -20,7 +17,7 @@ function writeOpenAIPackageJson(path: string, privacy: boolean) {
 
   // Write the modified package.json back to the file.
   const updatedPackageJsonString = JSON.stringify(packageJson, null, 2);
-  writeFileSync(packageJsonPath, updatedPackageJsonString, "utf8");
+  writeFileSync(path, updatedPackageJsonString, "utf8");
 
   console.log("Updated package.json to set private property to true.");
 }
@@ -30,7 +27,7 @@ const packages = getPackages(process.cwd());
 packages.then((packages) => {
   for (const p of packages.packages) {
     if (p.packageJson.name === "@gentrace/openai-v3") {
-      writeOpenAIPackageJson("../packages/openai-v3/package.json", false);
+      writeOpenAIPackageJson(`${p.dir}/package.json`, false);
     } else {
       writeOpenAIPackageJson(`${p.dir}/package.json`, true);
     }
