4ec3372933dec400bfac9ea12d31c0fd63c6703d
Vivek Nair <vivek@gentrace.ai>
2023-09-12 15:06:41 -0400

    fix: Export step run types/classes (#241)

--- packages/openai-v3/src/index.ts
+++ packages/openai-v3/src/index.ts
@@ -1,9 +1,19 @@
 import { SimpleOpenAIApi, OpenAIConfiguration } from "./handlers/simple";
 import { initPlugin, OpenAIPlugin } from "./plugin";
+import {
+  OpenAICreateChatCompletionStepRun,
+  OpenAICreateCompletionStepRun,
+  OpenAICreateEmbeddingStepRun,
+} from "./openai";
 
 export {
   initPlugin,
   OpenAIPlugin,
   SimpleOpenAIApi as OpenAIApi,
   OpenAIConfiguration as Configuration,
+
+  // Export step runs for custom integrations that might need it
+  OpenAICreateChatCompletionStepRun,
+  OpenAICreateCompletionStepRun,
+  OpenAICreateEmbeddingStepRun,
 };
--- packages/openai/src/index.ts
+++ packages/openai/src/index.ts
@@ -1,5 +1,19 @@
 import { SimpleOpenAI } from "./handlers/simple";
 import { initPlugin, OpenAIPlugin } from "./plugin";
+import {
+  OpenAICreateChatCompletionStepRun,
+  OpenAICreateCompletionStepRun,
+  OpenAICreateEmbeddingStepRun,
+} from "./openai";
 
-export { initPlugin, OpenAIPlugin, SimpleOpenAI as OpenAI };
+export {
+  initPlugin,
+  OpenAIPlugin,
+  SimpleOpenAI as OpenAI,
+
+  // Export step runs for custom integrations that might need it
+  OpenAICreateChatCompletionStepRun,
+  OpenAICreateCompletionStepRun,
+  OpenAICreateEmbeddingStepRun,
+};
 export default SimpleOpenAI;
