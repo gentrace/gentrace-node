123d742ce88a121c4869055527828ad880664cb0
Vivek Nair <vnair611@gmail.com>
2023-09-23 18:01:31 -0400

    fix: wip

--- packages/cli/src/entrypoint.js
+++ packages/cli/src/entrypoint.js
@@ -4,10 +4,10 @@ const React = require("react");
 const { config } = require("./utils.js");
 const importJsx = require("import-jsx");
 
-const CaseList = importJsx("./CaseList.js");
-const CaseCreate = importJsx("./CaseCreate.js");
-const ConfigGet = importJsx("./ConfigGet.js");
-const ConfigSet = importJsx("./ConfigSet.js");
+// const CaseList = importJsx("./CaseList.js");
+const CaseCreate = require("./CaseCreate.js");
+// const ConfigGet = importJsx("./ConfigGet.js");
+// const ConfigSet = importJsx("./ConfigSet.js");
 
 init({
   apiKey: config.apiKey ?? process.env.GENTRACE_API_KEY,
@@ -18,14 +18,14 @@ function Entrypoint({ command, options }) {
     case "cases-create":
       return <CaseCreate options={options} />;
 
-    case "cases-get":
-      return <CaseList />;
+    // case "cases-get":
+    //   return <CaseList />;
 
-    case "config-set":
-      return <ConfigSet options={options} />;
+    // case "config-set":
+    //   return <ConfigSet options={options} />;
 
-    case "config-get":
-      return <ConfigGet options={options} />;
+    // case "config-get":
+    //   return <ConfigGet options={options} />;
 
     default:
       return <Text>Unknown command</Text>;
