2553ea76ba374ea1ab0f698746a17f0236d6a03b
Vivek Nair <vnair611@gmail.com>
2023-10-03 14:20:58 -0400

    fix: wip

--- examples/evaluation/src/runner.ts
+++ examples/evaluation/src/runner.ts
@@ -1,6 +1,6 @@
 import { init, Pipeline, runTest } from "@gentrace/core";
 
-const PIPELINE_SLUG = "testing-html";
+const PIPELINE_SLUG = "testing3";
 
 init({
   apiKey: process.env.GENTRACE_API_KEY ?? "",
@@ -14,33 +14,44 @@ const pipeline = new Pipeline({
 
 async function submitTestRun() {
   try {
-    await runTest(PIPELINE_SLUG, async (testCase) => {
-      const runner = pipeline.start();
+    await runTest(
+      PIPELINE_SLUG,
+      async (testCase) => {
+        const runner = pipeline.start();
 
-      const outputs = await runner.measure(
-        (inputs) => {
-          console.log("inputs", inputs);
-          // Simply return inputs as outputs
-          return {
-            example:
-              "<h1>Example</h1><div>This is an <strong>example</strong></div>",
-          };
-        },
-        [testCase.inputs],
-        {
-          context: {
-            render: {
-              type: "html",
-              key: "example",
+        const outputs = await runner.measure(
+          (inputs) => {
+            console.log("inputs", inputs);
+            // Simply return inputs as outputs
+            return {
+              example:
+                "<h1>Example</h1><div>This is an <strong>example</strong></div>",
+            };
+          },
+          [testCase.inputs],
+          {
+            context: {
+              render: {
+                type: "html",
+                key: "example",
+              },
             },
           },
+        );
+
+        await runner.submit();
+
+        return [outputs, runner];
+      },
+      {
+        metadata: {
+          "test-run": {
+            type: "string",
+            value: "Some string value",
+          },
         },
-      );
-
-      await runner.submit();
-
-      return [outputs, runner];
-    });
+      },
+    );
   } catch (e) {
     console.error("Error value", e);
   }
