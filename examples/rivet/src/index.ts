93c164a52a74025ac6f19be750f8f92bb432f48b
Vivek Nair <vivek@gentrace.ai>
2023-10-10 14:52:47 -0400

    fix: Update Rivet Node package (#285)

--- examples/rivet/src/index.ts
+++ examples/rivet/src/index.ts
@@ -7,16 +7,13 @@ init({
   basePath: "http://localhost:3000/api/v1",
 });
 
-const RIVET_PROJECT_FILE = "./projects/chat.rivet-project";
-const RIVET_GRAPH_ID = "r97vYKQCVceae5VCCKK4J";
-
 async function processRivet() {
   console.log("Processing Rivet graph");
 
   const outputs = await runGraphInFile(
-    RIVET_PROJECT_FILE,
+    env.RIVET_PROJECT_FILE,
     {
-      graph: RIVET_GRAPH_ID,
+      graph: env.RIVET_GRAPH_ID,
       openAiKey: env.OPENAI_KEY as string,
       inputs: {
         messages: {
