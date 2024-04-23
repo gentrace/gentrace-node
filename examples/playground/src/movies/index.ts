bd10d06afff10c27613d5b685e5c6d7cbc00e7ed
xke <xke@groveflow.com>
2024-04-22 17:28:32 -0700

    feat: add playground movie example

---
+++ examples/playground/src/movies/index.ts
@@ -0,0 +1,30 @@
+import Movie, { allMovies } from "./movie";
+import movieQuery from "./movie-query";
+
+import { gentrace } from "./helpers";
+
+async function playground() {
+  gentrace.registerCustomType("movie");
+
+  allMovies.forEach((movie) => {
+    gentrace.registerCustomObject("movie", movie.title, movie);
+  });
+
+  gentrace.registerInteraction(
+    "Movie Query", // interaction name
+    {
+      movie: "movie",
+      question: "string",
+      sample_answer: "string",
+    }, // input fields
+    {
+      answer: "string",
+    }, // output fields
+    (inputs: { movie: Movie; question: string }) =>
+      movieQuery(inputs.movie, inputs.question), // interation
+  );
+
+  gentrace.start();
+}
+
+playground();
