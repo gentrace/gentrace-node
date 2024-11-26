cbb71dedc0231adb5d8ad6e744c01ad774ef3e48
Raissa Largman <raissa.largman@gmail.com>
2024-11-26 13:31:50 -0800

    feat: server webhook transport for experiment runner (#486)
    * feat: server webhook transport for experiment runner
    
    * fix: changeset
    
    * feat: add signature verification
    
    * feat: add enum parameter type

--- examples/evaluation/src/test-job-runner-webhook.ts
+++ examples/evaluation/src/test-job-runner-webhook.ts
@@ -1,13 +1,15 @@
 import {
   defineInteraction,
-  enumParameter,
   init,
-  listen,
+  handleWebhook,
   numericParameter,
   templateParameter,
+  enumParameter,
 } from "@gentrace/core";
 import OpenAI from "openai";
 import { z } from "zod";
+import express from "express";
+import { createHmac } from "crypto";
 
 init({
   apiKey: process.env.GENTRACE_API_KEY ?? "",
@@ -96,5 +98,58 @@ const chooseModel = defineInteraction({
   parameters: [modelParameter],
 });
 
-// Open a websocket connection to the Gentrace server
-listen();
+// Create Express app
+const app = express();
+
+// Verify the signature of the request
+app.use(
+  express.json({
+    verify: (req: any, res: express.Response, buf, next) => {
+      req.rawBody = buf;
+      const webhookSecret = process.env.GENTRACE_WEBHOOK_SECRET ?? "";
+
+      // Get the signature from the header
+      const signature = req.header("x-gentrace-signature");
+
+      if (!signature) {
+        throw new Error("No signature provided");
+      }
+
+      const calculatedSignature = `sha256=${createHmac("sha256", webhookSecret)
+        .update(req.rawBody)
+        .digest("hex")}`;
+
+      // Verify the signature
+      if (signature !== calculatedSignature) {
+        throw new Error("Invalid signature");
+      }
+    },
+  }),
+);
+
+// Add error handling middleware
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+  ) => {
+    res.status(401).send("Unauthorized");
+  },
+);
+
+// Add webhook endpoint
+app.post("/", async (req: express.Request, res: express.Response) => {
+  // Get the body of the request as a JSON object
+  const body = req.body;
+  await handleWebhook(body, (responseBody) => {
+    res.status(200).json(responseBody);
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 443;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
