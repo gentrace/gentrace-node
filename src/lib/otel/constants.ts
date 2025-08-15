bdab0456983079f48d54026f3206a4524c1829a0
joshlebed <31638080+joshlebed@users.noreply.github.com>
2025-08-15 19:14:36 +0000

    feat: add gentrace.in_experiment baggage key (#637)
    ### TL;DR
    
    Added support for tracking whether spans are part of an experiment with a new baggage attribute.
    
    ### What changed?
    
    - Added a new constant `ATTR_GENTRACE_IN_EXPERIMENT` to track when spans are part of an experiment
    - Modified the `_runEval` function to set this new attribute in the baggage
    - Updated the `GentraceSpanProcessor` to propagate both sampling and experiment attributes from baggage to spans
    - Created a `gentraceBaggageKeys` array to maintain the list of baggage keys that should be propagated to spans
    
    ### How to test?
    
    1. Run an experiment using the SDK
    2. Verify that spans created during the experiment have both the `gentrace.sample` and `gentrace.in_experiment` attributes set to "true"
    3. Check that these attributes are properly propagated through the span hierarchy
    
    ### Why make this change?
    
    This change enables better tracking of spans that are specifically part of experiments, allowing for improved filtering and analysis of experiment-related telemetry data. The distinction between regular sampling and experiment participation provides more context for understanding trace data.

--- src/lib/otel/constants.ts
+++ src/lib/otel/constants.ts
@@ -4,6 +4,14 @@
  */
 export const ATTR_GENTRACE_SAMPLE = 'gentrace.sample';
 
+
+/**
+ * Key used to identify spans that are part of a Gentrace experiment.
+ * When set to 'true', indicates that the span was created during an experiment run.
+ */
+
+export const ATTR_GENTRACE_IN_EXPERIMENT = 'gentrace.in_experiment';
+
 /**
  * Key used to identify which Gentrace pipeline a particular span belongs to.
  * Spans tagged with this attribute will be viewable in the "Traces" section of the Gentrace dashboard.
